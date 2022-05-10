481d852f946a97b86bbe7d10eda8ec4ea3770805
santiagoaluas <salulema@sistec.com.ec>
2022-05-09 20:00:21 -0500

    CORE 1

---
+++ src/components/Task.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Task = () => {
+  return (
+    <div>
+        <ul>
+            <li>Lear React</li>
+            <li>Climb mt. Everest</li>
+            <li>Run a marathon</li>
+            <li>Feed the dogs</li>
+        </ul>
+    </div>
+  )
+}
